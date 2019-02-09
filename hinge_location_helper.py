35cfc2a911f9281fd178f7de58b82b66663811ed
Justin Schechter <justin@nextdoorco.com>
2019-02-09 17:26:14 -0500

    Add insert sql builder

--- hinge_location_helper.py
+++ hinge_location_helper.py
@@ -5,3 +5,11 @@ def get_locs(ttc, btc, qty, hinge_ht, joh):
     for i in range(1, qty):
         hinges.append(top + (i * space))
     return tuple(hinges)
+
+
+def insert_builder(name, hinge_qty, hinge_ht, locs):
+    if hinge_qty == 3:
+        h4 = "NULL"
+    else:
+        h4 = locs[3]
+    return f"INSERT INTO hinge_locations (locations, hinge_qty, hinge_height, h1, h2, h3, h4) VALUES ('{name.upper()}', {hinge_qty}, {hinge_ht}, {locs[0]}, {locs[1]}, {locs[2]}, {h4});"
--- test_helper.py
+++ test_helper.py
@@ -9,3 +9,19 @@ def test_locs_three():
 def test_locs_four():
     result = h.get_locs(7.25, 12.25, 4, 4.5, 84)
     assert result == (5, 26.5, 48, 69.5)
+
+
+def test_insert_builder_three():
+    result = h.insert_builder("fcf", 3, 4.5, (5, 37.25, 69.5))
+    assert (
+        result
+        == "INSERT INTO hinge_locations (locations, hinge_qty, hinge_height, h1, h2, h3, h4) VALUES ('FCF', 3, 4.5, 5, 37.25, 69.5, NULL);"
+    )
+
+
+def test_insert_builder_four():
+    result = h.insert_builder("fcf", 4, 4.5, (5, 26.5, 48, 69.5))
+    assert (
+        result
+        == "INSERT INTO hinge_locations (locations, hinge_qty, hinge_height, h1, h2, h3, h4) VALUES ('FCF', 4, 4.5, 5, 26.5, 48, 69.5);"
+    )
